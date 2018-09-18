6a4f73cc26977bc433633587b8b9ef6c01edfc6c
McKenzie Fiege <mckenziefiege@McKenzies-MacBook-Pro.local>
2018-09-18 09:38:47 -0400

    Done

--- app.rb
+++ app.rb
@@ -2,5 +2,16 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+  get '/' do
+    erb :team
+  end
 
+  get '/newteam' do
+    # @finished = form
+    erb :team
+  end
+
+  post '/newteam' do
+    erb :newteam
+  end
 end
